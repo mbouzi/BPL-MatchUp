6eaa2eb85fac31a61c9499bdd7489f60327ef6e5
mbouzi <mrbouzi@gmail.com>
2016-03-08 17:00:42 -0500

    create teamThumbnail component

--- src/teamThumbnail.jsx
+++ src/teamThumbnail.jsx
@@ -1,1 +1,25 @@
-teamThumbnail.jsx
+var React = require('react')
+
+
+module.exports = React.createClass({
+
+	handleClick: function(){
+		this.props.whenItemClicked(this.props.team);
+	},
+
+
+	render: function(){
+		return (
+			<div className="col-xs-3 col-md-3" >
+				<a 
+					onClick={this.handleClick} href="#" 
+					className = {this.props.className}
+				>
+					<img 
+						src={this.props.crest} 
+					/>
+				</a>
+			</div>
+		)
+	}
+});
