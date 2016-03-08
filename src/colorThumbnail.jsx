3ee024aef872688bc38d5307c5d193f522ebdacc
mbouzi <mrbouzi@gmail.com>
2016-03-08 16:58:58 -0500

    create colorThumbnail component

--- src/colorThumbnail.jsx
+++ src/colorThumbnail.jsx
@@ -1,1 +1,19 @@
-colorThumbnail.jsx
+var React = require('react')
+
+
+module.exports = React.createClass({
+
+	handleClick: function(){
+		this.props.whenColorItemClicked(this.props.team);
+	},
+
+
+	render: function(){
+		return (
+			<a href="#" 
+				onClick={this.handleClick}
+				className={this.props.className}>
+			</a>
+		)
+	}
+});
